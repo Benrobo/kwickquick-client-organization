070da0d39f1a44c4639b1d9c86bd770b2fd0958c
benrobo <alumonabenaiah71@gmail.com>
2021-11-25 03:35:14 +0100

    added major changes to qrcodes and products

--- Dashboard/js/main.js
+++ Dashboard/js/main.js
@@ -62,6 +62,23 @@ const ProductsDetails = () => {
 
 }
 
+const createCountryCurrencies = async ()=>{
+    let currencyInp = $(".currencies");
+    let req = await fetch("js/currency.json");
+    let data = await req.json();
+
+    for (const res in data) {
+        let symbol = data[res].symbol;
+        let name = data[res].name;
+
+        let options = document.createElement("option");
+
+        options.value = symbol;
+        options.textContent = `${name} (${symbol})`;
+
+        currencyInp.append(options)
+    }
+}
 
 const ADD_PRODUCTS = async () => {
     let nameInp = $(".pName")
@@ -79,25 +96,12 @@ const ADD_PRODUCTS = async () => {
     let closeQrcodeModal = $(".close-qrcode-modal")
 
     // close modal
-    closeQrcodeModal.onclick = ()=>{
+    closeQrcodeModal.onclick = () => {
         qrcodeImageModal.style.visibility = "hidden"
     }
 
     // fill the select tag with all currencies
-    let req = await fetch("js/currency.json");
-    let data = await req.json();
-
-    for (const res in data) {
-        let symbol = data[res].symbol;
-        let name = data[res].name;
-
-        let options = document.createElement("option");
-
-        options.value = symbol;
-        options.textContent = `${name} (${symbol})`;
-
-        currencyInp.append(options)
-    }
+    createCountryCurrencies()
 
     // handle base64 image
     uploadBtn.onclick = (e) => {
@@ -171,13 +175,16 @@ const ADD_PRODUCTS = async () => {
                     text: result.productId,
                     width: 320,
                     height: 320,
-                    colorDark : "#000000",
-                    colorLight : "#ffffff",
-                    correctLevel : QRCode.CorrectLevel.H
+                    colorDark: "#000000",
+                    colorLight: "#ffffff",
+                    correctLevel: QRCode.CorrectLevel.H
                 });
             }
         } catch (e) {
-            return alert.error("Something went wrong")
+            alert.error("Something went wrong")
+            loading = false;
+            submitBtn.innerHTML = "Submit"
+            return
         }
 
     }
@@ -207,7 +214,7 @@ const QRCODE_PAGE = () => {
         qrcodeSidebar.style.width = "0px"
     }
 
-    
+
 
 
     if (qrcodeDetailsBtn.length > 1) {
@@ -226,19 +233,42 @@ const QRCODE_PAGE = () => {
 
 
                 mainQrcodeCont.innerHTML = `
-                  <div class="top">
-                      <img src="${img}" alt="qrcode" class="img-fluid">
-                      <a class="download btn" href="${img}" download="true">Download</a>
-                  </div>
-                  <div class="body">
-                      <p><b>Name:</b></p>
-                      <span>${itemName}</span>
-                      <p><b>Hash:</b></p>
-                      <span>${hash}</span>
-                      <p><b>Price</b></p>
-                      <span>${price}</span>
-                  </div>
+                    <div class="top">
+                        <div class="qrcode-img-cont"></div>
+
+                        <div class="action">
+                        <ion-icon name="download" class="download-qrcode download-btn"></ion-icon>
+                        </div>
+                        
+                    </div>
+                    <div class="body">
+                        <p><b>Name:</b></p>
+                        <span>${itemName}</span>
+                        <p><b>Hash:</b></p>
+                        <span>${hash}</span>
+                        <p><b>Price</b></p>
+                        <span>${price}</span>
+                    </div>
                 `
+                let qrcodeImgCont = $(".qrcode-img-cont");
+                let downloadQrcode = $(".download-qrcode");
+                let deleteBtn = $(".delete-qrcode");
+
+                generateQrcode(qrcodeImgCont, hash)
+
+                downloadQrcode.onclick = (e) => {
+                    console.log()
+                    let img = e.target.parentElement.parentElement.querySelector(".qrcode-img-cont img").src;
+                    let src = img;
+                    let a = document.createElement("a")
+                    a.href = src;
+                    a.download = "qrcode";
+                    a.click()
+                }
+
+                deleteBtn.onclick = (e) => {
+                    deleteQrcode(hash);
+                }
             }
         })
         // return
@@ -256,20 +286,181 @@ const QRCODE_PAGE = () => {
         let price = target.getAttribute("data-qrcode-price")
 
         mainQrcodeCont.innerHTML = `
-                  <div class="top">
-                      <img src="${img}" alt="qrcode" class="img-fluid">
-                      <a class="download btn" onclick="javacript:void()" href="javacript:void(${img})" download="true">Download</a>
-                  </div>
-                  <div class="body">
-                      <p><b>Name:</b></p>
-                      <span>${itemName}</span>
-                      <p><b>Hash:</b></p>
-                      <span>${hash}</span>
-                      <p><b>Price</b></p>
-                      <span>${price}</span>
-                  </div>
-                `
+        <div class="top">
+            <div class="qrcode-img-cont"></div>
+
+            <div class="action">
+
+            <ion-icon name="download" class="download-qrcode download-btn"></ion-icon>
+            </div>
+            
+        </div>
+        <div class="body">
+            <p><b>Name:</b></p>
+            <span>${itemName}</span>
+            <p><b>Hash:</b></p>
+            <span>${hash}</span>
+            <p><b>Price</b></p>
+            <span>${price}</span>
+        </div>
+      `
+        let qrcodeImgCont = $(".qrcode-img-cont");
+        let downloadQrcode = $(".download-qrcode");
+
+        generateQrcode(qrcodeImgCont, hash)
+
+        downloadQrcode.onclick = (e) => {
+            console.log()
+            let img = e.target.parentElement.parentElement.querySelector(".qrcode-img-cont img").src;
+            let src = img;
+            let a = document.createElement("a")
+            a.href = src;
+            a.download = "qrcode";
+            a.click()
+        }
     }
 
+    function deleteQrcode(hash) {
+        return async ()=>{
+            let senddata = {
+                pId: "b03e9dae-96ff-47e4-a867-9d6f454b336f",
+                orId: "b50b9c7c-1ba1-4665-b39f-f61865a49792"
+            }
 
+            let api = "http://localhost:5000/kwickquick/api/deleteQrcodeHash"
+            let req = await fetch(api, {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json",
+                    "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImM1YjkyMDQ5LWVlMjktNDEzYS1hNDIxLTY2NzJjYjk5NDRkZSIsImVtYWlsIjoidGVzbGEzQG1haWwuY29tIiwiaWF0IjoxNjM3NTE5OTI3LCJleHAiOjE2NjkwNzc1Mjd9.AK4kG0T9gM0KaxoqsaspX2EDTYq3P4bLuiHgomfKkIQ"
+                },
+                body: JSON.stringify(senddata)
+            })
+
+            let result = await req.json();
+
+            console.log(result)
+        }
+    }
+
+    function generateQrcode(elm, text){
+        return new QRCode(elm, {
+            text: text,
+            width: 200,
+            height: 200,
+            colorDark: "#000000",
+            colorLight: "#ffffff",
+            correctLevel: QRCode.CorrectLevel.H
+        });
+    }
+}
+
+
+// PRODUCTS PAGE
+const PRODUCTS_PAGE = ()=>{
+    let nameInp = $(".pName")
+    let priceInp = $(".pPrice")
+    let currencyInp = $(".currencies");
+    let uploadBtn = $(".upload")
+    let file = $(".imageInp");
+    let imgPreview = $(".product-image-preview");
+    let submitBtn = $(".submit-btn");
+    let chkImageEdit = $(".edit-image-chk");
+
+    let base64Image = "";
+    let loading = false;
+
+    createCountryCurrencies()
+
+
+    // handle base64 image
+    uploadBtn.onclick = (e) => {
+        file.click()
+    };
+
+    file.onchange = (e) => {
+        let newfile = file.files[0];
+        let imageSrc = URL.createObjectURL(newfile);
+        imgPreview.src = imageSrc
+        let reader = new FileReader()
+        reader.readAsDataURL(newfile);
+
+        reader.onload = function () {
+            base64Image = reader.result;
+        };
+    }
+
+    submitBtn.onclick = async (e) => {
+        if (nameInp.value === "" || priceInp.value === "") {
+            alert.error("fields cant be empty")
+            return
+        }
+        else {
+
+            let senddata = {
+                pName: nameInp.value,
+                pPrice: priceInp.value,
+                pCurrency: currencyInp.value,
+                orId: "somerandomid",
+            }
+            await sendData(senddata)
+        }
+    }
+
+    const sendData = async (data) => {
+        try {
+            loading = true;
+            submitBtn.innerHTML = "Loading ....."
+            submitBtn.classList.add("loading")
+            let req = await fetch("http://localhost:5000/kwickquick/api/addProducts", {
+                method: "post",
+                headers: {
+                    "content-type": "application/json",
+                    "authorization": `Bearer`
+                },
+                body: JSON.stringify(data)
+            })
+            let result = await req.json();
+
+            console.log(result)
+            if (result === "" || !result) {
+                qrcodeBox.innerHTML = "Product qrcode would be visible when products as been added."
+                return;
+            }
+            else if (req.status === 200) {
+                alert.success("Product added successfully")
+                loading = false;
+                submitBtn.innerHTML = "Submit"
+                submitBtn.classList.remove("loading")
+                // qrcodeBox.innerHTML = `
+                //     <img src="https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=${result.productId}" />
+                // `
+                qrcodeBox.innerHTML = ""
+                new QRCode(qrcodeBox, {
+                    text: result.productId,
+                    width: 320,
+                    height: 320,
+                    colorDark: "#000000",
+                    colorLight: "#ffffff",
+                    correctLevel: QRCode.CorrectLevel.H
+                });
+            }
+        } catch (e) {
+            alert.error("Something went wrong")
+            loading = false;
+            submitBtn.innerHTML = "Submit"
+            return
+        }
+
+    }
+
+    // edit image
+    console.log(chkImageEdit.checked)
+    chkImageEdit.onclick = (e)=>{
+        if(!chkImageEdit.checked) {
+            uploadBtn.style.display = "none"
+            return
+        };
+        uploadBtn.style.display = "flex"
+    }
 }
